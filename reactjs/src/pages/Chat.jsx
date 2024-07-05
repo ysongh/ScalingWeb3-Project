e87371dbc29731ee6c73a2fbd03b0b91e18c44fb
You Song Hou <ysongweb3@gmail.com>
2024-07-05 01:20:09 -0400

    feat: add a function to create an identity and key with XMTP

--- reactjs/src/pages/Chat.jsx
+++ reactjs/src/pages/Chat.jsx
@@ -1,9 +1,29 @@
-import { useClient } from '@xmtp/react-sdk';
+import { useEffect } from 'react';
+
+import { useClient, Client } from '@xmtp/react-sdk';
 
 function Chat({ userSigner }) {
   const { client, error, isLoading, initialize } = useClient();
 
   console.log(client, initialize, userSigner);
+
+  useEffect(() => {
+    if (userSigner) initXmtp();
+  }, [userSigner])
+
+  const initXmtp = async () => {
+    const options = {  
+      env: "dev"  
+    };
+
+    const keys = await Client.getKeys(userSigner, {  
+      ...options,  
+      skipContactPublishing: true,  
+      persistConversations: false,  
+    });  
+    console.log(keys);
+  }
+  
 
   if (error) {
     return "An error occurred while initializing the client";
