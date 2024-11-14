73b505897ba811bfbe76e995f576e39acd896d85
You Song Hou <ysongweb3@gmail.com>
2024-11-14 02:07:23 -0500

    feat: add Wallet component

--- reactjs/onchainkit/src/App.jsx
+++ reactjs/onchainkit/src/App.jsx
@@ -1,12 +1,15 @@
-import { Providers } from "./providers"
+import { Providers } from "./providers";
+
+import { WalletComponent } from "./Wallet";
 
 function App() {
 
   return (
     <Providers>
       <h1>OnchainKit</h1>
+      <WalletComponent /> 
     </Providers>
   )
 }
 
-export default App
+export default App;
