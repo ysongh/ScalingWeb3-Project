724eae68a008190c5402c15d05399eb3ab107fd5
You Song Hou <ysongweb3@gmail.com>
2024-11-13 22:05:18 -0500

    feat: integrate tanstack/react-query

--- reactjs/onchainkit/src/providers.jsx
+++ reactjs/onchainkit/src/providers.jsx
@@ -1,21 +1,25 @@
 import { OnchainKitProvider } from '@coinbase/onchainkit';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { base } from 'wagmi/chains'; // add baseSepolia for testing
 import { useState } from 'react';
-import {  WagmiProvider } from 'wagmi';
+import { WagmiProvider } from 'wagmi';
  
 import { getConfig } from './wagmi'; // your import path may vary
  
 export function Providers(props) {
   const [config] = useState(() => getConfig());
+  const [queryClient] = useState(() => new QueryClient());
  
   return (
     <WagmiProvider config={config} initialState={props.initialState}>
-      <OnchainKitProvider
-        apiKey={import.meta.env.VITE_PUBLIC_ONCHAINKIT_API_KEY}
-        chain={base} // add baseSepolia for testing
-      >
-        {props.children}
-      </OnchainKitProvider>
+      <QueryClientProvider client={queryClient}>
+        <OnchainKitProvider
+          apiKey={import.meta.env.VITE_PUBLIC_ONCHAINKIT_API_KEY}
+          chain={base} // add baseSepolia for testing
+        >
+          {props.children}
+        </OnchainKitProvider>
+      </QueryClientProvider>
     </WagmiProvider>
   );
 }
