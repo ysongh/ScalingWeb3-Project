ab0ba28d659cbb4b85c6647c484f05fea56810c2
You Song Hou <ysongweb3@gmail.com>
2024-11-27 22:35:58 -0500

    style: update text input UI

--- reactjs/onchainkit/src/components/DisplayENS.jsx
+++ reactjs/onchainkit/src/components/DisplayENS.jsx
@@ -6,9 +6,27 @@ function DisplayENS() {
 
   return (
     <>
-      <label>Enter Address: </label>
-      <input value={val} onChange={e => setVal(e.target.value)} />
-      <Name address={val}/>
+      <div className="bg-white shadow-md rounded-lg w-[500px] p-6">
+        <h2 className="text-xl font-bold text-gray-800 mb-2">Find ENS name</h2>
+        <div className="relative">
+          <input
+            type="text"
+            value={val}
+            onChange={e => setVal(e.target.value)}
+            className="w-full px-4 py-3 prounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 pt-4 pb-2"
+            placeholder="0x0"
+          />
+          <label
+            htmlFor="name"
+            className={`absolute top-4 left-4 text-gray-500 transition-all duration-300 ${
+              val ? 'text-xs -translate-y-3' : 'text-base'
+            }`}
+          >
+            Enter Address:
+          </label>
+        </div>
+        <Name address={val}/>
+      </div>
     </>
   )
 }
