30f4301517411493d1d340a5e369f525dd3cd81a
You Song Hou <ysongweb3@gmail.com>
2024-12-01 01:17:51 -0500

    feat: instantiate Turnkey

--- nodejs/turnkey/index.js
+++ nodejs/turnkey/index.js
@@ -3,6 +3,16 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const { Turnkey } = require("@turnkey/sdk-server");
+
+const turnkey = new Turnkey({
+  apiBaseUrl: "https://api.turnkey.com",
+  apiPrivateKey: process.env.TURNKEY_API_PRIVATE_KEY,
+  apiPublicKey: process.env.TURNKEY_API_PUBLIC_KEY,
+  defaultOrganizationId: process.env.TURNKEY_ORGANIZATION_ID,
+});
+
+const apiClient = turnkey.apiClient();
 
 const app = express();
 
