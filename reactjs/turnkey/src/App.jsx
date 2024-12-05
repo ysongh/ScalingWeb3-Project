b780f81d3c4c98ec1d954fd9aed29d50b294eb6c
You Song Hou <ysongweb3@gmail.com>
2024-12-05 01:13:24 -0500

    feat: instantiate Turnkey using the Browser SDK

--- reactjs/turnkey/src/App.jsx
+++ reactjs/turnkey/src/App.jsx
@@ -1,4 +1,16 @@
+import { Turnkey } from "@turnkey/sdk-browser";
+
+import { TURNKEY_ORGANIZATION_ID } from "./keys";
+
 function App() {
+  const turnkey = new Turnkey({
+    apiBaseUrl: "https://api.turnkey.com",
+    defaultOrganizationId: TURNKEY_ORGANIZATION_ID,
+  });
+  
+  const passkeyClient = turnkey.passkeyClient();
+  console.log(passkeyClient);
+  
   return (
     <>
       <h1>First time using Turnkey </h1>
