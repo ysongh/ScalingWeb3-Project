08b4ff3906b0e163353dfd294f8359bfaf5492e9
You Song Hou <ysongweb3@gmail.com>
2024-12-09 01:28:08 -0500

    refactor: move JavaScript Browser Turnkey SDK code to its own component

--- reactjs/turnkey/src/App.jsx
+++ reactjs/turnkey/src/App.jsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
-import { Turnkey } from "@turnkey/sdk-browser";
 import { TurnkeyProvider } from "@turnkey/sdk-react";
 
+import BrowserSDK from "./BrowserSDK";
 import { TURNKEY_ORGANIZATION_ID } from "./keys";
 
 const turnkeyConfig = {
@@ -15,39 +14,10 @@ const turnkeyConfig = {
 }
 
 function App() {
-  const [name, setName] = useState("");
-  const [displayName, setDisplayName] = useState("");
-
-  const turnkey = new Turnkey({
-    apiBaseUrl: "https://api.turnkey.com",
-    defaultOrganizationId: TURNKEY_ORGANIZATION_ID,
-  });
-  
-  const passkeyClient = turnkey.passkeyClient();
-  console.log(passkeyClient);
-
-  const createPasskeyCredential = async () => {
-    const credential = await passkeyClient.createUserPasskey({
-      publicKey: {
-        user: {
-          name: name,
-          displayName: displayName
-        }
-      }
-    });
-
-    console.log(credential);
-  }
-  
   return (
     <TurnkeyProvider config={turnkeyConfig}>
       <h1>First time using Turnkey </h1>
-      <h2>Create Passkey Credential</h2>
-      <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Name"/>
-      <input value={displayName} onChange={(e) => setDisplayName(e.target.value)} placeholder="Display Name"/>
-      <button onClick={createPasskeyCredential}>
-        Create
-      </button>
+      <BrowserSDK />
     </TurnkeyProvider>
   )
 }
---
+++ reactjs/turnkey/src/BrowserSDK.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { Turnkey } from "@turnkey/sdk-browser";
+
+import { TURNKEY_ORGANIZATION_ID } from "./keys";
+
+function BrowserSDK() {
+  const [name, setName] = useState("");
+  const [displayName, setDisplayName] = useState("");
+
+  const turnkey = new Turnkey({
+    apiBaseUrl: "https://api.turnkey.com",
+    defaultOrganizationId: TURNKEY_ORGANIZATION_ID,
+  });
+  
+  const passkeyClient = turnkey.passkeyClient();
+  console.log(passkeyClient);
+
+  const createPasskeyCredential = async () => {
+    const credential = await passkeyClient.createUserPasskey({
+      publicKey: {
+        user: {
+          name: name,
+          displayName: displayName
+        }
+      }
+    });
+
+    console.log(credential);
+  }
+
+  return (
+    <div>
+      <h2>Create Passkey Credential</h2>
+      <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Name"/>
+      <input value={displayName} onChange={(e) => setDisplayName(e.target.value)} placeholder="Display Name"/>
+      <button onClick={createPasskeyCredential}>
+        Create
+      </button>
+    </div>
+  )
+}
+
+export default BrowserSDK;
