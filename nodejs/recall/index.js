40c4ff922080c9ef553ef37a3698514e7336bc1a
You Song Hou <ysongweb3@gmail.com>
2025-03-14 00:06:27 -0400

    feat: add API to display testnet data for Recall

--- nodejs/recall/index.js
+++ nodejs/recall/index.js
@@ -3,12 +3,22 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const { testnet } = require("@recallnet/chains");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
+
+app.get('/testnet', async (req, res) => {
+  try {
+    res.json({ testnet });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.get('/', (req, res) => res.send('It Work'));
 
